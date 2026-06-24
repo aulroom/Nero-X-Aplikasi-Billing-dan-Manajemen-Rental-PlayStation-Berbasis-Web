4fbf81b0472bf100941bce834a2f6a9f4a74ec73
aulroom <aulyaramadhanssage@gmail.com>
2026-06-25 00:16:43 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,22 @@
 const { Sequelize } = require('sequelize');
 
-// Koneksi ke MySQL tanpa password
-const sequelize = new Sequelize('nero_x', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+// Ambil environment (Render akan memakai 'development' sesuai setting kita tadi)
+const env = process.env.NODE_ENV || 'development';
+
+// Baca file config.json
+const config = require('./config.json')[env];
+
+// Buat koneksi menggunakan data dari config.json
+const sequelize = new Sequelize(
+  config.database, 
+  config.username, 
+  config.password, 
+  {
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect,
+    dialectOptions: config.dialectOptions
+  }
+);
 
 module.exports = sequelize;
