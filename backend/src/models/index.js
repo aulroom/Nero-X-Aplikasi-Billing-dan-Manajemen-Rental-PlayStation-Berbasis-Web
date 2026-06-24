bff5f7c726d6aa91a3bb8000ed08e4763dc4cfa5
aulroom <aulyaramadhanssage@gmail.com>
2026-06-25 00:00:09 +0700

    Update index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,9 +1,10 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../../config/db');
 
-const Ruangan = require('./ruangan')(sequelize, DataTypes);
-const Barang = require('./barang')(sequelize, DataTypes);
-const Peminjaman = require('./peminjaman')(sequelize, DataTypes);
+// PERBAIKAN: Huruf awal disesuaikan menjadi kapital persis seperti nama file
+const Ruangan = require('./Ruangan')(sequelize, DataTypes);
+const Barang = require('./Barang')(sequelize, DataTypes);
+const Peminjaman = require('./Peminjaman')(sequelize, DataTypes);
 
 // Setup relasi
 Ruangan.associate({ Peminjaman });
